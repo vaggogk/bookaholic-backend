2184497813503b2a48c08c10286589ffd43da80b
vaggogk <vaggogk@gmail.com>
2025-11-05 18:48:34 +0200

    feat: - Implement all IUserService methods with proper business logic

--- src/main/java/com/bookaholic/service/UserService.java
+++ src/main/java/com/bookaholic/service/UserService.java
@@ -1,5 +1,6 @@
 package com.bookaholic.service;
 
+import com.bookaholic.model.User;
 import com.bookaholic.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -7,12 +8,39 @@ import org.springframework.stereotype.Service;
 
 
 @Service
-public class UserService  implements IUserService{
-
+public class UserService  implements IUserService {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
 
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public boolean existsByUsername(String username) {
+        return false;
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return false;
+    }
 }
 
