18905a6f6e4a5c2aaf4442a1bb24f4d7d9d060ee
vaggogk <vaggogk@gmail.com>
2025-11-06 20:35:15 +0200

    feat: add user login class and error message

--- src/main/java/com/bookaholic/service/UserService.java
+++ src/main/java/com/bookaholic/service/UserService.java
@@ -4,6 +4,7 @@ import com.bookaholic.model.User;
 import com.bookaholic.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -11,36 +12,64 @@ import org.springframework.stereotype.Service;
 public class UserService  implements IUserService {
 
     @Autowired
-    private UserRepository userRepository;
+    private  UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
 
     @Override
     public User findByUsername(String username) {
-        return null;
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found with username: " + username));
     }
 
     @Override
     public User findById(Long id) {
-        return null;
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
     }
 
     @Override
     public User findByEmail(String email) {
-        return null;
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Email not found" + email));
     }
 
     @Override
     public User save(User user) {
-        return null;
+        if (existsByUsername(user.getUsername())) {
+            throw new RuntimeException("❌ Username already exists");
+        }
+        if (existsByEmail(user.getEmail())) {
+            throw new RuntimeException("📧 Email already exists");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
     }
 
     @Override
     public boolean existsByUsername(String username) {
-        return false;
+        return userRepository.existsByUsername(username);
     }
 
     @Override
     public boolean existsByEmail(String email) {
-        return false;
+        return userRepository.existsByEmail(email);
     }
+
+    @Override
+    public User login(String username, String password) {
+        User user = findByUsername(username);
+
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+
+        return  user;
+    }
+
 }
 
