d33979e0f22a7d767b720d23a4f5668991585297
vaggogk <vaggogk@gmail.com>
2025-11-06 20:34:47 +0200

    feat: add AllArgsConstructor

--- src/main/java/com/bookaholic/repository/UserRepository.java
+++ src/main/java/com/bookaholic/repository/UserRepository.java
@@ -2,9 +2,10 @@ package com.bookaholic.repository;
 
 import com.bookaholic.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     //  Check if username exists
     Optional<User> findByUsername(String username);
