2284e190acd1231017c70fd7d965217291e82fb7
vaggogk <vaggogk@gmail.com>
2025-11-21 18:00:16 +0200

    feat: add new ExceptionHandler

--- src/main/java/com/bookaholic/Exception/ErrorHandler.java
+++ src/main/java/com/bookaholic/Exception/ErrorHandler.java
@@ -2,8 +2,11 @@ package com.bookaholic.Exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 @Slf4j
@@ -13,4 +16,17 @@ public class ErrorHandler {
     public  ResponseEntity<String> handleError(RuntimeException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        log.error("Validation error: {}", errorMessage);
+        return ResponseEntity.badRequest().body(errorMessage);
+    }
 }
+
